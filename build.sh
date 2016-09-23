142cb9e7db7c0646824d2e8b8fa8b30ac656b0fa
shellrock <hanwoody@gmail.com>
2016-09-23 17:56:54 +0800

    build images

---
+++ build.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+set -e
+
+REG_URL=index.csphere.cn
+IREG_URL=docker.nicescale.com
+TAGS="2.5.3 2.5"
+export BASE_IMAGE=tomcat:8-jre8-alpine
+
+docker pull $BASE_IMAGE
+
+IMAGE=microimages/dubbo-producer-demo
+docker build -t $IMAGE service-producer
+docker tag -f $IMAGE $REG_URL/$IMAGE
+docker tag -f $IMAGE $IREG_URL/$IMAGE
+for t in $TAGS; do
+  docker tag -f $IMAGE $REG_URL/$IMAGE:$t
+  docker tag -f $IMAGE $IREG_URL/$IMAGE:$t
+  docker tag -f $IMAGE $IMAGE:$t
+done
+docker push $IMAGE
+docker push $REG_URL/$IMAGE
+docker push $IREG_URL/$IMAGE
+
+IMAGE=microimages/dubbo-consumer-demo
+docker build -t $IMAGE service-consumer
+docker tag -f $IMAGE $REG_URL/$IMAGE
+docker tag -f $IMAGE $IREG_URL/$IMAGE
+for t in $TAGS; do
+  docker tag -f $IMAGE $REG_URL/$IMAGE:$t
+  docker tag -f $IMAGE $IREG_URL/$IMAGE:$t
+  docker tag -f $IMAGE $IMAGE:$t
+done
+docker push $IMAGE
+docker push $REG_URL/$IMAGE
+docker push $IREG_URL/$IMAGE
